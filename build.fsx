fe6abd80c2fb3f7f96a63c1169203462c27e9409
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-31 10:58:51 +0100

    Project settings

--- build.fsx
+++ build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
+let gitOwner = "forki"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "PaketIntro"
 
 open FsReveal
 open Fake
